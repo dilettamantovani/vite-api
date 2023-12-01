835e30d81320234203a64cb70b27d55b6f5e897e
dilettits <mantovani.d3@gmail.com>
2023-12-01 10:19:08 +0100

    added v-for

--- src/App.vue
+++ src/App.vue
@@ -7,11 +7,25 @@ export default {
   components: {
     BreweryCard
   },
+
   data() {
     return {
       store,
     }
   },
+
+  mounted() {
+    this.breweriesList();
+    console.log(this.store)
+  },
+
+  methods: {
+    breweriesList() {
+      axios.get(this.store.breweriesAPI).then(result => {
+        this.store.breweries = result.data;
+      })
+    }
+  }
 }
 
 </script>
@@ -20,9 +34,14 @@ export default {
   <main>
     <h1>Scotland's Best Breweries</h1>
     <div id="wrapper">
-      <BreweryCard />
+      <BreweryCard v-for="brewery in store.breweries" :brewery="brewery" />
     </div>
   </main>
 </template>
 
-<style scoped></style>
+<style scoped>
+#wrapper {
+  display: flex;
+  flex-wrap: wrap;
+}
+</style>
--- src/components/BreweryCard.vue
+++ src/components/BreweryCard.vue
@@ -10,14 +10,31 @@ export default {
 
 <template>
     <div class="card">
-        <h2>Birreria</h2>
-        <span><b>Nazione</b></span><br>
-        <span><b>città</b></span><br>
-        <span><b>CAP</b></span><br>
-        <span><b>telefono</b></span><br>
-        <span><b>sito</b></span><br>
+        <h2>{{ brewery.name }}</h2>
+        <h4>Country</h4>
+        <span>{{ brewery.country }}</span>
+        <h4>City</h4>
+        <span>{{ brewery.city }}</span>
+        <h4>Postal Code</h4>
+        <span>{{ brewery.postal_code }}</span>
+        <h4>Phone Number</h4>
+        <span>{{ brewery.phone }}</span>
+        <h4>Website</h4>
+        <span>{{ brewery.website_url }}</span>
     </div>
 </template>
 
 
-<style scoped></style>
+<style scoped>
+.card {
+    width: calc(100% / 5 - 6rem);
+    background-color: rgb(215, 191, 145);
+    border-radius: 8px;
+    margin: 2rem;
+    padding: 3rem;
+}
+
+h4 {
+    margin: 0.4rem 0 0 0;
+}
+</style>
