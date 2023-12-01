fbeb042c26d7f11f3c608c83e9670c28db7619fe
dilettits <mantovani.d3@gmail.com>
2023-12-01 09:21:41 +0100

    created card

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,18 @@
-<script setup>
+<script>
 import axios from 'axios';
+import { store } from './store.js'
+import BreweryCard from './components/BreweryCard.vue';
+
+export default {
+  components: {
+    BreweryCard
+  },
+  data() {
+    return {
+      store,
+    }
+  },
+}
 
 </script>
 
@@ -7,7 +20,7 @@ import axios from 'axios';
   <main>
     <h1>Scotland's Best Breweries</h1>
     <div id="wrapper">
-      <card />
+      <BreweryCard />
     </div>
   </main>
 </template>
---
+++ src/components/BreweryCard.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+    name: 'BreweryCard',
+    props: {
+        brewery: Object
+    }
+}
+</script>
+
+
+<template>
+    <div class="card">
+        <h2>Birreria</h2>
+        <span><b>Nazione</b></span><br>
+        <span><b>città</b></span><br>
+        <span><b>CAP</b></span><br>
+        <span><b>telefono</b></span><br>
+        <span><b>sito</b></span><br>
+    </div>
+</template>
+
+
+<style scoped></style>
